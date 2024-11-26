367230eea5519ebc06acc7055a594b146a2af0dc
MvLx <andimuhmuflih21@gmail.com>
2024-11-26 08:46:13 +0800

    push 3 memperbaiki tampilan course dan logika all course

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import addIconSelectors from `@iconify/tailwindcss`;
 
 /** @type {import('tailwindcss').Config} */
 export default {
